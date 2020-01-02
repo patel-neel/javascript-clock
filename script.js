44796aaf771a303a123d57c6451aa0dae63016ba
Neel Patel <321neel@gmail.com>
2020-01-01 22:57:53 -0500

    finished hour hand. Fixed 00s transition bug.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
-const secondHand = document.querySelector('.second-hand')
-const minutesHand = document.querySelector('.min-hand')
+const secondHand = document.querySelector('.second-hand');
+const minutesHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 function setDate(){
     const now = new Date()
@@ -14,6 +15,15 @@ function setDate(){
     const minutesDegrees = ((minutes / 60) * 360) + 90;
     minutesHand.style.transform = `rotate(${minutesDegrees}deg)`
     
+    const hour = now.getHours();
+    const hourDegree = ((hour / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegree}deg)`
+
+    if(seconds === 0){
+        secondHand.style.transition = 'none'
+    }else{
+        secondHand.style.transition = "all 0.5s";
+    }
     
 }
 
