e7bdc144ddecd3caf2e9ac45610d464171f49a26
Neel Patel <321neel@gmail.com>
2020-01-01 22:35:37 -0500

    minutes and seconds hands working

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('testing');
+
+const secondHand = document.querySelector('.second-hand')
+const minutesHand = document.querySelector('.min-hand')
+
+function setDate(){
+    const now = new Date()
+    const seconds = now.getSeconds();
+    
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`
+
+    
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    minutesHand.style.transform = `rotate(${minutesDegrees}deg)`
+    
+    
+}
+
+setInterval(setDate,1000)
